fdedf27fa6580492be7243c6c9ad1c85212d5f9a
John Martin <johnny@iamjohnnym.com>
2019-10-15 03:30:35 -0700

    Update runaction to include files based on shabang
    Instead of requiring file names, require files contain a shabang.

--- runaction.sh
+++ runaction.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 cd "$GITHUB_WORKSPACE" || exit 1
-find . -name \*.sh -exec shellcheck {} +
-find . -name \*.bashrc -exec shellcheck {} +
-find . -name \*.bash_profile -exec shellcheck {} +
+for FILE in $(find . -type f ! -iname 'Makefile' -exec \
+grep '/usr/bin/env bash' {} + | awk -F':' '{print $$1}') ; do \
+    echo "Checking: ${FILE}" ; \
+    shellcheck "${FILE}" ; \
+done
